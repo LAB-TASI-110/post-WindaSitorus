42238165d28d1e9a5387e87e6a9efff1fa027ec1
WindaSitorus <windasitorus0197@gmail.com>
2026-04-28 10:04:10 +0700

    sudah menyelesaikan

--- src/driver/Driver1.java
+++ src/driver/Driver1.java
@@ -1,7 +1,55 @@
 package driver;
 
+import java.util.Scanner;
+import model.Model1;
+
 public class Driver1 {
 	public static void main(String[] args) {
-		
+		Scanner input = new Scanner(System.in);
+		Model1 model = new Model1();
+
+		System.out.println("=== Sistem Pengiriman Del-Express ===");
+
+		while (true) {
+			System.out.print("Kode kota tujuan: ");
+			String kode = input.next().toUpperCase();
+
+			if (kode.equals("END")) {
+				break;
+			}
+
+			System.out.print("Berat paket Butet (kg): ");
+			double beratButet = input.nextDouble();
+
+			if (!model.isValidKode(kode)) {
+				System.out.println("Kode kota tidak tersedia.\n");
+				continue;
+			}
+
+			String kota = model.getKota(kode);
+			double ongkirPerKg = model.getOngkir(kode);
+			String keterangan = model.getKeterangan(kode);
+
+			double beratUcok = model.hitungBeratUcok(beratButet);
+			double totalBerat = model.hitungTotalBerat(beratButet, beratUcok);
+			double totalOngkir = model.hitungOngkir(totalBerat, ongkirPerKg);
+			String promo = model.getPromo(totalBerat, keterangan);
+
+			// apply diskon jika ada
+			if (totalBerat > 10) {
+				totalOngkir = totalOngkir * 0.9;
+			}
+
+			System.out.println("\n=== Struk Pembayaran Del-Express ===");
+			System.out.println("Kota tujuan        : " + kota);
+			System.out.println("Berat paket Butet  : " + beratButet + " kg");
+			System.out.println("Berat paket Ucok   : " + beratUcok + " kg");
+			System.out.println("Total berat        : " + totalBerat + " kg");
+			System.out.println("Total ongkos kirim : Rp" + (int) totalOngkir);
+			System.out.println("Promo diperoleh    : " + promo);
+			System.out.println();
+		}
+
+		input.close();
 	}
 }
--- src/model/Model1.java
+++ src/model/Model1.java
@@ -1,5 +1,75 @@
 package model;
 
 public class Model1 {
-	
+
+	public boolean isValidKode(String kode) {
+		return kode.equals("MDN") || kode.equals("BLG") ||
+			   kode.equals("JKT") || kode.equals("SBY");
+	}
+
+	public String getKota(String kode) {
+		switch (kode) {
+			case "MDN": return "Medan";
+			case "BLG": return "Balige";
+			case "JKT": return "Jakarta";
+			case "SBY": return "Surabaya";
+			default: return "";
+		}
+	}
+
+	public double getOngkir(String kode) {
+		switch (kode) {
+			case "MDN": return 8000;
+			case "BLG": return 5000;
+			case "JKT": return 12000;
+			case "SBY": return 13000;
+			default: return 0;
+		}
+	}
+
+	public String getKeterangan(String kode) {
+		switch (kode) {
+			case "MDN":
+			case "BLG":
+				return "Dalam Pulau";
+			case "JKT":
+			case "SBY":
+				return "Luar Pulau";
+			default:
+				return "";
+		}
+	}
+
+	public double hitungBeratUcok(double beratButet) {
+		return (3.0 / 2.0) * beratButet;
+	}
+
+	public double hitungTotalBerat(double butet, double ucok) {
+		return butet + ucok;
+	}
+
+	public double hitungOngkir(double totalBerat, double ongkirPerKg) {
+		return totalBerat * ongkirPerKg;
+	}
+
+	public String getPromo(double totalBerat, String keterangan) {
+		String promo = "";
+
+		if (totalBerat > 10) {
+			promo += "Diskon ongkir sebesar 10%";
+		}
+
+		if (keterangan.equals("Luar Pulau")) {
+			if (!promo.equals("")) {
+				promo += ", ";
+			}
+			promo += "Asuransi gratis";
+		}
+
+		if (promo.equals("")) {
+			promo = "Tidak mendapat promo";
+		}
+
+		return promo;
+	}
 }
